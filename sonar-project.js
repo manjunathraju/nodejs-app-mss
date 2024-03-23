fe56d7a4f098193b3483a7e854d7a5bef5857204
manjunathraju <61818998+manjunathraju@users.noreply.github.com>
2024-03-24 00:41:13 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://23.254.226.235:9000/',
+  serverUrl: 'http://23.254.201.122:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '79fa5c2e3221ea68dd117445d73513acfb444a6c',
+	    'sonar.login': '1c5547bf5308c6cdcef786362fa839fcab8e725b',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
